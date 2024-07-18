e81ff49b2ce467f0a18ee93ff0e5344a580ef6d0
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-07-18 22:38:24 +0700

    feat handlePayment

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,27 +1,36 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Roles } from 'src/decorator/role.decorator';
 import { OrderService } from './order.service';
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
+import { Role } from 'src/enums/role.enum';
+import { AccessTokenGuard } from 'src/auth/access-token.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Order } from './entities/order.entity';
 
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @Roles(Role.Admin, Role.User, Role.Manager, Role.Employee)
+  @UseGuards(AccessTokenGuard, RolesGuard)
   @Post()
-  create() {
-    return this.orderService.create();
-  }
-
-  @Get()
-  findAll() {
-    return this.orderService.findAll();
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.orderService.create(createOrderDto);
   }
 
   @Get(':id')
@@ -29,13 +38,48 @@ export class OrderController {
     return this.orderService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.orderService.update(+id);
-  }
-
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.orderService.remove(+id);
+  }
+}
+
+@Controller('admin/order')
+@Roles(Role.Admin, Role.Manager, Role.Employee)
+@UseGuards(AccessTokenGuard, RolesGuard)
+export class OrderAdminController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateOrderDto: UpdateOrderDto,
+  ) {
+    return this.orderService.update(id, updateOrderDto);
+  }
+
+  @Patch('update-status/:id')
+  updateOrderStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateOrderStatusDto: UpdateOrderStatusDto,
+  ) {
+    return this.orderService.updateOrderStatus(id, updateOrderStatusDto);
+  }
+
+  @Get()
+  async findAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+    @Query('name') name = '',
+  ): Promise<Pagination<Order>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.orderService.findAll(
+      {
+        page,
+        limit,
+        route: `${process.env.SERVER}/admin/order`,
+      },
+      name,
+    );
   }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,21 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Like, Raw, Repository } from 'typeorm';
+import { OrderItem } from './entities/orderItem.entity';
+import { HttpService } from '@nestjs/axios';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class OrderService {
-  create() {
-    return 'This action adds a new order';
+  constructor(
+    @InjectRepository(Order)
+    private ordersRepo: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private orderItemsRepo: Repository<OrderItem>,
+    private readonly httpService: HttpService,
+  ) {}
+  async create(createOrderDto: CreateOrderDto) {
+    const { orderItems } = createOrderDto;
+    const order = await this.ordersRepo.save(createOrderDto);
+
+    const newOrderItems = orderItems.map((o) => ({ ...o, orderId: order.id }));
+    await this.orderItemsRepo.save(newOrderItems);
+    return order;
   }
 
-  findAll() {
-    return `This action returns all order`;
+  async findAll(
+    options: IPaginationOptions,
+    name: string,
+  ): Promise<Pagination<Order>> {
+    return paginate<Order>(this.ordersRepo, options, {
+      where: [
+        {
+          id: Raw((alias) => `CAST(${alias} as char(20)) Like '%${name}%'`),
+        },
+        {
+          fullName: Like(`%${name}%`),
+        },
+        {
+          user: {
+            username: Like(`%${name}%`),
+          },
+        },
+      ],
+      relations: {
+        orderItems: true,
+        user: true,
+      },
+      order: {
+        updatedDate: 'DESC',
+      },
+    });
   }
 
   findOne(id: number) {
     return `This action returns a #${id} order`;
   }
 
-  update(id: number) {
-    return `This action updates a #${id} order`;
+  async update(id: number, updateOrderDto: UpdateOrderDto) {
+    const exist = await this.ordersRepo.findOneBy({ id });
+    if (!exist) {
+      throw new NotFoundException('Order not found.');
+    }
+
+    const { orderItems } = updateOrderDto;
+    await this.orderItemsRepo.save(orderItems);
+    return this.ordersRepo.save({ id, ...updateOrderDto });
+  }
+
+  async updateOrderStatus(id: number, updateOrderStatus: UpdateOrderStatusDto) {
+    const exist = await this.ordersRepo.findOneBy({ id });
+    if (!exist) {
+      throw new NotFoundException('Order not found.');
+    }
+
+    return this.ordersRepo.update(id, { ...updateOrderStatus });
   }
 
   remove(id: number) {
